c1099719579a25478a97c9bf8e8bfb2319b77ec4
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-06-30 10:48:24 +0300

    0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,28 @@
 """
 
 def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the given number of rows.
+
+    Args:
+        n (int): The number of rows to generate in Pascal's Triangle.
+
+    Returns:
+        list: The generated Pascal's Triangle as a nested list of integers.
+
+    """
     if n <= 0:
         return []
 
+    # Start with the first row containing only 1
     triangle = [[1]]
 
     for i in range(1, n):
+        # Generate the current row based on the previous row
         row = [1] + [
-                triangle[i - 1][j] + triangle[i - 1][j + 1]
-                for j in range(i-1)] + [1]
+            triangle[i - 1][j] + triangle[i - 1][j + 1]
+            for j in range(i - 1)
+        ] + [1]
         triangle.append(row)
 
     return triangle
