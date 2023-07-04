31f3a8576f1d3b465142c6a6368b002a903e54cb
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-04 16:54:32 +0300

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
 
-
 def canUnlockAll(boxes):
-    """
-    Check if all the boxes can be opened.
-
-    Args:
-        boxes (list): List of lists representing the boxes and their corresponding keys.
-
-    Returns:
-        bool: True if all boxes can be opened, False otherwise.
-    """
     n = len(boxes)  # Number of boxes
     visited = [False] * n  # Keep track of visited boxes
     visited[0] = True  # Mark the first box as visited
@@ -21,19 +11,8 @@ def canUnlockAll(boxes):
         keys = boxes[box]  # Get the keys inside the current box
 
         for key in keys:
-            if 0 <= key < n and not visited[key]:
+            if key < n and not visited[key]:
                 visited[key] = True  # Mark the box as visited
                 queue.append(key)  # Add the box to the queue
 
     return all(visited)
-
-
-if __name__ == '__main__':
-    boxes = [[1], [2], [3], [4], []]
-    print(can_unlock_all(boxes))  # Output: True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(can_unlock_all(boxes))  # Output: True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(can_unlock_all(boxes))  # Output: False
