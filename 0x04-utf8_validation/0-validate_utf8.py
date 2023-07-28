6b40e6957fdb4d19d0b320616057554f51ad5a85
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-28 13:40:21 +0300

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,7 +3,6 @@
 UTF-8 Validation Module
 """
 
-
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding.
@@ -49,10 +48,17 @@ def validUTF8(data):
     idx = 0
     while idx < len(data):
         num_bytes = get_num_bytes(data[idx])
-        if num_bytes == 0:
-            return False  # Invalid byte
 
+        # Invalid UTF-8 character: More bytes than needed
+        if num_bytes == 0 or idx + num_bytes > len(data):
+            return False
+
+        # Check if the next bytes are valid
         if not check_next_bytes(idx, num_bytes - 1):
+            return False
+
+        # Invalid UTF-8 character: Value greater than 0x10FFFF
+        if num_bytes == 4 and (data[idx] & 0x07) >= 0x05:
             return False
 
         idx += num_bytes
@@ -71,4 +77,11 @@ if __name__ == "__main__":
     print(validUTF8(data))  # True
 
     data = [229, 65, 127, 256]
+    print(validUTF8(data))  # False
+
+    # Additional test cases
+    data = [235, 140]
+    print(validUTF8(data))  # False
+
+    data = [345, 467]
     print(validUTF8(data))  # False
