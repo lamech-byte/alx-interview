1176857de99fc83f29a27efbc9531240451bfc3c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-14 21:13:40 +0300

    0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Rotate a given n x n 2D matrix 90 degrees clockwise.
+
+    Args:
+        matrix (list[list[int]]): The n x n 2D matrix to rotate.
+
+    Returns:
+        None: The matrix is edited in-place.
+    """
+    n = len(matrix)
+    
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - 1 - j][i]
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
+            matrix[j][n - 1 - i] = temp
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    for row in matrix:
+        print(row)
