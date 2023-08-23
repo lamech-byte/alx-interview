1b9bd8d7017e82f076ebab23922f3f6bffea1bde
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-23 11:24:52 +0300

    0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for val in range(1, total + 1):
+        min_coins = float('inf')
+        for coin in coins:
+            if coin <= val:
+                min_coins = min(min_coins, dp[val - coin] + 1)
+        dp[val] = min_coins
+
+    if dp[total] == float('inf'):
+        return -1
+    return dp[total]
+
+if __name__ == "__main__":
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
