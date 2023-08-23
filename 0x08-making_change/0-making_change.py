b33ecf90cb9af56e51ee34a5b26455a070ef1e27
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-23 12:10:26 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,23 +21,6 @@ def makeChange(coins, total):
          If the total is 0 or less, returns 0.
          If the total cannot be met by any combination of the available
          coins, returns -1.
-
-    Algorithm:
-    This function utilizes dynamic programming to solve the coin change
-    problem.
-    It builds an array 'dp' where dp[i] represents the minimum number
-    of coins
-    needed to achieve the amount 'i'. It iterates through the coin
-    values and updates
-    the 'dp' array incrementally to calculate the minimum coins required
-    for each amount.
-    The final result is stored in dp[total].
-
-    Example:
-    >>> makeChange([1, 2, 25], 37)
-    7
-    >>> makeChange([1256, 54, 48, 16, 102], 1453)
-    -1
     """
 
     if total <= 0:
