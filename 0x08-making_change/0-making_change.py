8c9f9368b000f4b1af050a7445678d79ef7ac1ac
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-23 11:48:02 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,21 +4,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
+    # Initialize an array to store the minimum number of coins needed for each total
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for val in range(1, total + 1):
-        min_coins = float('inf')
-        for coin in coins:
-            if coin <= val:
-                min_coins = min(min_coins, dp[val - coin] + 1)
-        dp[val] = min_coins
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     if dp[total] == float('inf'):
         return -1
-    return dp[total]
-
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))
-    print(makeChange([1256, 54, 48, 16, 102], 1453))
+    else:
+        return dp[total]
