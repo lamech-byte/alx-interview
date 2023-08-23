b66a8b191cfac4dcb53ae84d9c77cb2d9b01315c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-23 11:26:04 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,6 +18,7 @@ def makeChange(coins, total):
         return -1
     return dp[total]
 
+
 if __name__ == "__main__":
     print(makeChange([1, 2, 25], 37))
     print(makeChange([1256, 54, 48, 16, 102], 1453))
