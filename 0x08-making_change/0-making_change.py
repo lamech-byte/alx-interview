dd16b92db07b482bf593f5a7231471292081eb90
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-23 11:56:29 +0300

    0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,31 @@
 #!/usr/bin/python3
+"""
+0-making_change.py
+Module to calculate the fewest number of coins needed to
+meet a given amount total.
+"""
 
 def makeChange(coins, total):
+    """
+    Calculate the fewest number of coins needed to meet a given
+    amount total.
+
+    Args:
+    coins (list): List of coin values available for making change.
+    total (int): The target amount to be achieved using the 
+    available coins.
+
+    Returns:
+    int: The fewest number of coins needed to meet the total amount.
+         If the total is 0 or less, returns 0.
+         If the total cannot be met by any combination of the
+         available coins, returns -1.
+
+    Algorithm:
+    This function utilizes dynamic programming to solve the coin
+    change problem.
+    """
+
     if total <= 0:
         return 0
 
@@ -16,3 +41,9 @@ def makeChange(coins, total):
         return -1
     else:
         return dp[total]
+
+# Example usage
+if __name__ == "__main__":
+    coins = [1, 2, 25]
+    total_amount = 37
+    print("Minimum coins needed:", makeChange(coins, total_amount))
