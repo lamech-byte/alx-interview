4569de296e77f9e2ee3f50f3e44b53bc2e25d3e4
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-31 07:59:37 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
 
-
 def island_perimeter(grid):
-    perimeter = 0
+    """
+    Calculate the perimeter of an island described by the given grid.
 
-    """ Start with the assumption that the current land cell
-                contributes 4 sides to the perimeter """
+    Args:
+    grid (list of list of int): A grid representing the island, where
+    0 represents water and 1 represents land.
+
+    Returns:
+    int: The perimeter of the island.
+
+    Notes:
+    - Cells are connected horizontally/vertically (not diagonally).
+    - The grid is rectangular, with width and height not exceeding 100.
+    - The grid is completely surrounded by water.
+    - There is only one island (or nothing).
+    - The island doesn’t have "lakes" (water inside that isn’t connected to
+    the water surrounding the island).
+    """
+
+    perimeter = 0
+    
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                """ Check neighbors (up, down, left, right) and subtract 1 from
-                perimeter for each neighboring land cell """
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
                 if i < len(grid) - 1 and grid[i + 1][j] == 1:
@@ -21,5 +35,5 @@ def island_perimeter(grid):
                     perimeter -= 1
                 if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
-
+    
     return perimeter
