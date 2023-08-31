a03b1c094e2186d2994ff6e01c4a535db9254429
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-31 08:00:37 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island described by the given grid.
@@ -21,7 +22,7 @@ def island_perimeter(grid):
     """
 
     perimeter = 0
-    
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
@@ -35,5 +36,5 @@ def island_perimeter(grid):
                     perimeter -= 1
                 if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
-    
+
     return perimeter
