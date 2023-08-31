b92b3a5fb95a31f3cd81483a62d4055607f92c2c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-08-31 07:55:43 +0300

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,20 +3,15 @@
 def island_perimeter(grid):
     perimeter = 0
 
-
-"""
-Start with the assumption that the current land cell contributes
-4 sides to the perimeter
-"""
+    """ Start with the assumption that the current land cell
+                contributes 4 sides to the perimeter """
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
                 perimeter += 4
                 
-              """
-              Check neighbors (up, down, left, right) and subtract
-              1 from perimeter for each neighboring land cell
-              """
+                """ Check neighbors (up, down, left, right) and subtract 1 from
+                perimeter for each neighboring land cell """
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
                 if i < len(grid) - 1 and grid[i + 1][j] == 1:
