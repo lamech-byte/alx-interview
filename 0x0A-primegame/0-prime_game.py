3fc9143e225e2628b6c41d7e8bb35ea8ca382fdd
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-09-07 18:52:29 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,63 +1,36 @@
 #!/usr/bin/python3
 
-def is_prime(num):
-    """
-    Check if a number is prime.
-
-    Args:
-        num (int): The number to check.
-
-    Returns:
-        bool: True if the number is prime, False otherwise.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+""" Prime Game """
 
 
 def isWinner(x, nums):
     """
-    Determine the winner of a game between Maria and Ben.
-
     Args:
-        x (int): The number of rounds to play.
-        nums (list of int): A list of integers representing the
-        set of numbers for each round.
-
+        x: the number of rounds
+        nums: an array of n
     Returns:
-        str or None: The name of the player that won the most rounds
-        (either "Maria" or "Ben").
-                     If the winner cannot be determined, returns None.
-
-    Constraints:
-        - x and len(nums) will not be larger than 10,000.
+        name of the player that won the most rounds
     """
-    if x <= 0 or not nums:
+    if not nums or x < 1:
         return None
-
-    # Calculate the maximum number in nums
-    max_num = max(nums)
-
-    # Precompute primes using the Sieve of Eratosthenes
-    primes = [False, False] + [True] * (max_num - 1)
-    for p in range(2, int(max_num**0.5) + 1):
-        if primes[p]:
-            for i in range(p * p, max_num + 1, p):
-                primes[i] = False
-
-    # Count the number of primes in nums
-    prime_count = sum(1 for num in nums if primes[num])
-
-    # Determine the winner based on the prime count
-    if prime_count % 2 == 0:
-        return "Ben"
-    else:
+    n = max(nums)
+    primes = [True for i in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not primes[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            primes[j] = False
+    primes[0] = primes[1] = False
+    c = 0
+    for i in range(len(primes)):
+        if primes[i]:
+            c += 1
+        primes[i] = c
+    player1 = 0
+    for n in nums:
+        player1 += primes[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-
-
-# Example usage
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+    return "Ben"
